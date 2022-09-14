a013ecfa8e1b0508c734d1cdce24473cf62a7c86
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-09-14 14:12:47 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -14,18 +14,5 @@ int _isalpha(int c)
 	return (1);
 	}
 	else
-	{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + ('0');
-	_putchar('\n');
 	return (0);
-	}
 }
