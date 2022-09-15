4fbc3adecd440f561f9ec3e19cceb742f4f23abf
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-09-15 03:02:58 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,9 +20,9 @@ int main(void)
 			hold1 = (bk1 + bk2) / LARGEST;
 			hold2 = (bk1 + bk2) % LARGEST;
 			hold3 = fr1 + fr2 + hold1;
-			fr1 = fr2, fr2, = hold3;
+			fr1 = fr2, fr2 = hold3;
 			bk1 = bk2, bk2 = hold2;
-			printf("%0101lu", fr2, bk2);
+			printf("%lu%010lu", fr2, bk2);
 		}
 		else
 		{
