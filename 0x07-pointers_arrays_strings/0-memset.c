b4642e5f0bf2fccba306f5c87e0325152606d3e5
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-09-28 22:13:08 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,8 +5,7 @@
  * @s: memory space to be filled
  * @b: byte to fill with
  * @n: number of spaces to fill
-
-
+ *
  * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
