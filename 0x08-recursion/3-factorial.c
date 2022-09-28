b70ff9d9596f9d2ffe9af0206c3f99d205710ac2
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-09-28 20:00:44 +0000

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * factorial - find the factorial of n
+ * @n: Number to find the factorial of
+ *
+ * Return: The value of the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+}
