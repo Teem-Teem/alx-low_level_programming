e4819bb6b2b4c9f32e94ff556a1407f0c8fd5410
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-10-02 22:27:19 +0000

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file for prototype
- * void prototype (void)
- * int prototype (int)
- */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
