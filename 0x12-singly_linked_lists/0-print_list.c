e07e6eeeb598281fe09850a5c009a78bffc16f09
Oluwatimilehin <olaniyioluwatimilehin@gmail.com>
2022-10-21 15:27:09 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nelem;
 
-	i = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
